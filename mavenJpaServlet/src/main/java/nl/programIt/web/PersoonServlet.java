b3d3c5e2d341fbe2da8b20b368a51990f63ad17a
hamidi71 <chiahna71@gmail.com>
2016-08-23 15:55:44 +0200

    Jpa en servlet en jsp

--- mavenJpaServlet/src/main/java/nl/programIt/web/PersoonServlet.java
+++ mavenJpaServlet/src/main/java/nl/programIt/web/PersoonServlet.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package nl.programIt.web;
 
 import java.io.IOException;
@@ -18,10 +16,17 @@ import javax.servlet.http.HttpServletResponse;
 public class PersoonServlet extends HttpServlet{
 	//get en post methoden zit in de class HttpServlet
 	@Override
-	protected void doGet(HttpServletRequest vraag, HttpServletResponse aantwoord)
+	protected void doGet(HttpServletRequest vraag, HttpServletResponse aantword)
 			throws ServletException, IOException {
+		//HttpServletRequest ontvangen(vraag)
+		//HttpServletResponse verzenden(aantword)
 		String naam=vraag.getParameter("naam");
-		System.out.println(naam);
+		int leeftijd=Integer.valueOf(vraag.getParameter("leeftijd"));
+		System.out.println("naam is: "+ naam + "en leeftijd is: "+ leeftijd);
+		vraag.setAttribute("VoorNaam", naam);
+		vraag.setAttribute("leeftijd", leeftijd);
+		vraag.getRequestDispatcher("home.jsp").forward(vraag, aantword);
+		
 		
 		/*PrintWriter exit =aantwoord.getWriter();
 		exit.println("salaam")*/;
